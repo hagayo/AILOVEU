eee9cb37ee0a0ca947e16fc183fd33950242a4f2
Hagay Onn <hagayo@gmail.com>
2024-04-27 10:14:53 +0300

    Update ailoveu.js

--- ailoveu.js
+++ ailoveu.js
@@ -25,8 +25,8 @@ function dir(locale) {
 async function fetchTranslationsFor(newLocale) {
     const localePath = `./_locales/${newLocale}/messages.json`;
     console.log(localePath);
-    import translationsData from localePath;
-    console.log(translationsData);
+    import data from localePath assert { type: 'json' };
+    console.log(data);
     // let translationJson = require(`./_locales/${newLocale}/messages.json`);
     // console.log(translationJson);
     // alert(translationJson);
