2e690dd831bb4b77b72e6f10bea5b92d6bab7362
Hagay Onn <hagayo@gmail.com>
2024-05-04 07:35:24 +0300

    Update ailoveu.js

--- ailoveu.js
+++ ailoveu.js
@@ -1,1 +1,104 @@
-const defaultLocale="en",defaultAlign="ltr",supportedLocales=["en","he","fr"],divs=document.querySelectorAll("div[align-left]");let locale="en",translations={};async function setLocale(t){if(t===locale)return;const e=await fetchTranslationsFor(t);locale=t,translations=e,document.documentElement.lang=t,langDirection=dir(t),langDirection!=defaultAlign&&(alert("language direction changed: "+langDirection),document.documentElement.dir=langDirection,"ltr"===langDirection?setAlignLeft():setAlignRight()),translatePage()}function setAlignRight(){for(const t of divs)t.setAttribute("align-right",""),t.removeAttribute("align-left")}function setAlignLeft(){for(const t of divs)t.setAttribute("align-left",""),t.removeAttribute("align-right")}function dir(t){return"he"===t?"rtl":"ltr"}async function fetchTranslationsFor(t){const e=`/locales/${t}.json`,n=await fetch(e);return await n.json()}function translatePage(){document.querySelectorAll("[data-i18n-key]").forEach(translateElement)}function translateElement(t){const e=t.getAttribute("data-i18n-key"),n=translations[e];t.innerText=n}function bindLocaleSwitcher(t){const e=document.querySelector("[data-i18n-switcher]");e.value=t,e.onchange=t=>{setLocale(t.target.value)}}function isSupported(t){return supportedLocales.indexOf(t)>-1}function supportedOrDefault(t){return t.find(isSupported)||"en"}function browserLocales(t=!1){return navigator.languages.map((e=>t?e.split("-")[0]:e))}document.addEventListener("DOMContentLoaded",(()=>{const t=supportedOrDefault(browserLocales(!0));setLocale(t),bindLocaleSwitcher(t)}));
+// The default locale our website show when loading (English)
+const defaultLocale = "en";
+const defaultAlign = "ltr";
+const supportedLocales = ["en", "he", "fr"];
+const divs = document.querySelectorAll('div[align-left]');
+
+let locale = defaultLocale;     // active locale, set by user preferences
+let translations = {};          // Gets filled with active locale translations
+
+// Load translations and translate the page for given locale
+async function setLocale(newLocale) {
+    if (newLocale === locale) return;
+    const newTranslations = await fetchTranslationsFor(newLocale);
+    locale = newLocale;
+    translations = newTranslations;
+    document.documentElement.lang = newLocale;
+    langDirection = dir(newLocale);
+    if (langDirection != defaultAlign) {
+        alert("language direction changed: " + langDirection);
+        document.documentElement.dir = langDirection;
+        if (langDirection === "ltr") {
+            setAlignLeft();
+        } else {
+            setAlignRight();
+        }
+    }
+    translatePage();
+}
+
+function setAlignRight(){
+    for (const div of divs) {
+        div.setAttribute('align-right', '');
+        div.removeAttribute('align-left');
+    }
+}
+function setAlignLeft(){
+    for (const div of divs) {
+        div.setAttribute('align-left', '');
+        div.removeAttribute('align-right');
+    }
+}
+
+function dir(locale) {
+    return locale === "he" ? "rtl" : "ltr";
+}
+
+// Retrieve translations JSON object for the given locale
+async function fetchTranslationsFor(newLocale) {
+    // return await fetch(`https://ailoveu.art/locales/${newLocale}/messages.json`).then(res => res.json()).then(console.log)
+    const localePath = `/locales/${newLocale}.json`;
+    const response = await fetch(localePath);
+    return await response.json();
+}
+
+// Replace inner text of all elements with data-i18n-key attribute with their corresponding key translation 
+function translatePage() {
+    document.querySelectorAll("[data-i18n-key]").forEach(translateElement);
+}
+
+// Replace inner text of given HTML element to active locale translation
+function translateElement(element) {
+    const key = element.getAttribute("data-i18n-key");
+    const translation = translations[key];
+    element.innerText = translation;
+}
+
+// Detect and Translate page to user’s preferred locale when page content ready
+document.addEventListener("DOMContentLoaded", () => {
+    const initialLocale = supportedOrDefault(browserLocales(true));
+    setLocale(initialLocale);
+    bindLocaleSwitcher(initialLocale);
+});
+
+// update the page when the user selects new locale
+function bindLocaleSwitcher(initialValue) {
+    const switcher = document.querySelector("[data-i18n-switcher]");
+    switcher.value = initialValue;
+    switcher.onchange = (e) => {
+        // Set the locale to the selected option[value]
+        setLocale(e.target.value);
+    };
+}
+
+function isSupported(locale) {
+    return supportedLocales.indexOf(locale) > -1;
+}
+
+// return first supported locale from locales-array or return default locale
+function supportedOrDefault(locales) {
+    return locales.find(isSupported) || defaultLocale;
+}
+
+/**
+ * Retrieve user-preferred locales from the browser
+ * browserLocales() will return ["fr-CA", "zh-CN"]
+ * browserLocales(true) will return ["fr", "zh"] instead.
+ * param {boolean} languageCodeOnly - when true, returns ["en", "fr"] instead of ["en-US", "fr-FR"]
+ * returns array | undefined
+ */
+function browserLocales(languageCodeOnly = false) {
+    return navigator.languages.map((locale) =>
+        languageCodeOnly ? locale.split("-")[0] : locale, 
+    );
+}
